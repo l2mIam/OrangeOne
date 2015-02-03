2e756df5c33f4911e37960dd89bdb68295612d59
dyhansen <Monkeysdoom@gmail.com>
2015-02-02 20:02:47 -0800

    Updated 2D Map

--- js/index.js
+++ js/index.js
@@ -12,8 +12,8 @@ window.requestAnimFrame = (function(){
 var canvas = document.createElement('canvas'),
     ctx = canvas.getContext('2d');
 
-canvas.width = 300;
-canvas.height = 300;
+canvas.width = 640;
+canvas.height = 352;
 
 document.body.appendChild(canvas);
 
@@ -23,7 +23,7 @@ var keys = {};
 
 window.addEventListener('keydown', function(e) {
     keys[e.keyCode] = true;
-    
+
 });
 window.addEventListener('keyup', function(e) {
     delete keys[e.keyCode];
@@ -42,7 +42,7 @@ var Camera = function() {
     this.x = (canvas.width / 2) - entity.x;
     this.y = (canvas.height / 2) - entity.y;
   }
-  
+
   this.getPosition = function(entity) {
     this.x = (canvas.width / 2) - entity.x;
     this.y = (canvas.height / 2) - entity.y;
@@ -52,7 +52,7 @@ var Camera = function() {
 var ent = function() {
     this.load = false;
     this.imgX = 0;
-    
+
     this.sprite = function(src, srcX, srcY, dtx, dty, x, y, width, height, speed) {
             this.src = src;
             this.srcX = srcX;
@@ -64,52 +64,52 @@ var ent = function() {
             this.width = width;
             this.height = height;
             this.speed = speed;
-         
+
             this.image = new Image();
             this.image.src = src;
-        
+
     this.render = function() {
-        ctx.drawImage(this.image, this.srcX, this.srcY, this.dtx, this.dty, this.x, this.y, this.width, this.height); 
+        ctx.drawImage(this.image, this.srcX, this.srcY, this.dtx, this.dty, this.x, this.y, this.width, this.height);
       }
     }
-    
+
     this.update = function() {
     }
-    
+
     this.move = function() {
-        if(87 in keys) { // W 
+        if(87 in keys) { // W
             this.spriteRoll(512, 8);
             this.y -= this.speed;
         }
-        
+
         if(83 in keys) { // S
             this.spriteRoll(640, 8);
             this.y += this.speed;
         }
-        
+
         if(65 in keys) { // A
             this.spriteRoll(576, 8);
             this.x -= this.speed;
         }
-        
+
         if(68 in keys) { // D
             this.spriteRoll(704, 8);
             this.x += this.speed;
         }
-        
+
     }
-    
+
     this.spriteRoll = function(srY, maxLength) {
         this.srcY = srY;
         this.imgX += 1;
-            
+
         this.srcX = this.dtx * this.imgX;
-            
+
         if (this.imgX >= maxLength) {
-            this.imgX = 0;   
+            this.imgX = 0;
         }
     }
-    
+
     this.bounds = function() {
     this.x = m.clamp(this.x, 0 - this.width/2 + 20, 30000 - this.width/2);
     this.y = m.clamp(this.y, 0 - this.height/2 + 20, 30000 - this.height/2);
@@ -118,34 +118,34 @@ var ent = function() {
 
 var player = new ent();
               // src, srcX, srcY, dtx, dty, x, y, width, height, speed
-player.sprite("http://opengameart.org/sites/default/files/red_orc.png", 0, 640, 64, 64, 100, 100, 62, 62, 5);
+player.sprite("http://opengameart.org/sites/default/files/red_orc.png", 0, 640, 64, 64, 300, 300, 62, 62, 5);
 
 player.image.onload = function() {
-        player.load = true;   
+        player.load = true;
 }
 
 var tiles = function() {
   // Width and height of each tile
-  this.TILE_WIDTH = 30;
-  this.TILE_HEIGHT = 30;
-  
-  this.x = 0; 
+  this.TILE_WIDTH = 32;
+  this.TILE_HEIGHT = 32;
+
+  this.x = 0;
   this.y = 0;
   this.width = 60;
   this.height = 60;
-  
+
   var rc = Math.floor((Math.random() * 200) + 0);
   var gc = Math.floor((Math.random() * 200) + 0);
   var bc = Math.floor((Math.random() * 200) + 0);
   var ac = 1;
-  
+
   // Draw the tile to the position (sx, sy)
   this.draw = function() {
     this.sx = 0;
     this.sy = 0;
     this.sx = this.x * this.TILE_WIDTH;
     this.sy = this.y * this.TILE_HEIGHT;
-    
+
     ctx.fillStyle = "rgba(" + rc + ", " + gc + ", " + bc + ", " + ac + ")";
     ctx.fillRect(this.sx, this.sy, this.width, this.height);
   }
@@ -155,10 +155,10 @@ var Map = function() {
   // Rows (x), colums Y
   this.ROWS = 1000;
   this.COLS = 1000;
-  
+
   this.tiles = this.ROWS * this.COLS;
   this.tilesArray = [];
-  
+
   this.map = function() {
     // Creates 1000 Colums
     for (var y = 0; y < this.COLS; y++) {
@@ -172,7 +172,7 @@ var Map = function() {
       }
     }
   }
-  
+
   this.tiles = function(cam, tWidth, tHeight) {
     for (var y = 0; y < this.COLS; y++) {
       for (var x = 0; x < this.ROWS; x++) {
@@ -181,8 +181,8 @@ var Map = function() {
 					this.tilesArray[y*this.COLS + x].x * tWidth < -cam.x + canvas.width &&
 					this.tilesArray[y*this.COLS + x].y * tHeight < -cam.y + canvas.width &&
 					(this.tilesArray[y*this.COLS + x].x + 1) * tWidth > -cam.x &&
-					(this.tilesArray[y*this.COLS + x].y + 1) * tHeight > -cam.y 
-				   ) 
+					(this.tilesArray[y*this.COLS + x].y + 1) * tHeight > -cam.y
+				   )
 				{
             // draw tile
 					this.tilesArray[y*this.COLS + x].draw();
@@ -196,36 +196,36 @@ var game = function() {
     this.m = new Map();
     this.t = new tiles();
     this.cam  = new Camera();
-  
+
     this.start = function() {
       this.m.map();
       this.cam.camera(player);
-      this.loop();   
+      this.loop();
     }
-    
+
     this.loop = function() {
         this.update();
         this.render();
         requestAnimFrame(this.loop.bind(this));
     }
-    
+
     this.update = function() {
       this.cam.getPosition(player);
       player.bounds();
       player.move();
     }
-    
+
     this.render = function() {
      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      
+
      ctx.save();
-      
+
      ctx.translate(this.cam.x, this.cam.y);
      this.m.tiles(this.cam, this.t.TILE_WIDTH, this.t.TILE_HEIGHT);
       if (player.load) {
         player.render();
       }
-      
+
      ctx.restore();
     }
 }
