13d9c3794d53460a85ee73512e61cf9ed4a176de
kngrace <kngrace@uw.edu>
2015-03-06 17:59:19 -0800

    Re-adding in support for arrow keys and enter key (Loren's idea)

--- js/game.js
+++ js/game.js
@@ -28,6 +28,11 @@ var W_KEY = 87;
 var S_KEY = 83;
 var A_KEY = 65;
 var D_KEY = 68;
+var LEFT_KEY  = 37;
+var UP_KEY    = 38;
+var RIGHT_KEY = 39;
+var DOWN_KEY  = 40;
+var ENTER_KEY = 13;
 var SPACE_KEY = 32;
 var DEBUG_KEY = 192;  // ` key ~ key  is the debug key
 
@@ -230,7 +235,7 @@ var Sprite = function() {
         var exit; // tracks if the player's movement has triggered an exit and zone change
 
         /** Checks which keys are being currently pressed and moves player if new location is valid. */
-        if(W_KEY in keys) {
+        if(W_KEY in keys || UP_KEY in keys) {
           // W
             this.spriteRoll(512, 8, clockTick, 0.1); // even if player doesn't move, animate them!
 
@@ -266,7 +271,7 @@ var Sprite = function() {
             }
         }
 
-        if(S_KEY in keys) { // S
+        if(S_KEY in keys || DOWN_KEY in keys) { // S
             this.spriteRoll(640, 8, clockTick, 0.1); // even if player doesn't move, animate them!
 
             /** Determine if moving the player will result in entering a new grid location*/
@@ -299,7 +304,7 @@ var Sprite = function() {
             }
         }
 
-        if(A_KEY in keys) { // A
+        if(A_KEY in keys || LEFT_KEY in keys) { // A
             this.spriteRoll(576, 8, clockTick, 0.1); // even if player doesn't move, animate them!
 
             /** Determine if moving the player will result in entering a new grid location*/
@@ -332,7 +337,7 @@ var Sprite = function() {
             }
         }
 
-        if(D_KEY in keys) { // D
+        if(D_KEY in keys || RIGHT_KEY in keys) { // D
             this.spriteRoll(704, 8, clockTick, 0.1); // even if player doesn't move, animate them!
 
             /** Determine if moving the player will result in entering a new grid location*/
@@ -787,11 +792,11 @@ npc_Map1Blocker.update = function(clockTick) {
     npc_Map1Blocker.dialog[0] = "Would you mind not blocking my path?";
   }
   if(player.y < 171) {
-    if(W_KEY in keys) {
+    if(W_KEY in keys || UP_KEY in keys) {
       this.spriteRoll(512, 8,  clockTick, 0.1);
       this.y = (player.y - 30);
     }
-    if(S_KEY in keys) {
+    if(S_KEY in keys || DOWN_KEY in keys) {
       this.spriteRoll(640, 8,  clockTick, 0.1);
       this.y = (player.y - 30);
     }
@@ -1231,7 +1236,7 @@ var Controller = function() {
                     interactionButton.width, interactionButton.height);
             }
 
-            if (W_KEY in keys) {
+            if (W_KEY in keys || UP_KEY in keys) {
                 wButton.src = './img/pressed_W.png';
                 topctx.drawImage(wButton, 0, 0, wButton.width, wButton.height,
                     (wButton.width * 1.5),
@@ -1245,7 +1250,7 @@ var Controller = function() {
                     wButton.width, wButton.height);
             }
 
-            if (S_KEY in keys) {
+            if (S_KEY in keys || DOWN_KEY in keys) {
                 sButton.src = './img/pressed_S.png';
                 topctx.drawImage(sButton, 0, 0, sButton.width, sButton.height,
                     (sButton.width * 1.5),
@@ -1257,7 +1262,7 @@ var Controller = function() {
                     topcanvas.height - sButton.height, sButton.width, sButton.height);
             }
 
-            if (A_KEY in keys) {
+            if (A_KEY in keys || LEFT_KEY in keys) {
                 aButton.src = './img/pressed_A.png';
                 topctx.drawImage(aButton, 0, 0, aButton.width, aButton.height,
                     (aButton.width * .5),
@@ -1270,7 +1275,7 @@ var Controller = function() {
                     topcanvas.height - (aButton.height * 1.5),
                     aButton.width, aButton.height);
             }
-            if (D_KEY in keys) {
+            if (D_KEY in keys || RIGHT_KEY in keys) {
                 dButton.src = './img/pressed_D.png';
                 topctx.drawImage(dButton, 0, 0, dButton.width, dButton.height,
                     (dButton.width * 2.5),
@@ -1406,17 +1411,17 @@ var Game = function() {
     this.handleKeyDown = function (key_id) {
 
         // 87, 83, 65, 68, 32
-        if (key_id === SPACE_KEY) { // Spacebar
+        if (key_id === SPACE_KEY || key_id === ENTER_KEY) { // Spacebar
             player.interact(interactNPC);
             console.log("space");
             //g.loadZone(2, 1, 8); // kirsten debug, tested zone loading via button press
-        } else if (key_id === W_KEY) {
+        } else if (key_id === W_KEY || key_id === UP_KEY) {
             player.facing = "north";
-        } else if (key_id === A_KEY) {
+        } else if (key_id === A_KEY || key_id === LEFT_KEY) {
             player.facing = "west";
-        } else if (key_id === S_KEY) {
+        } else if (key_id === S_KEY || key_id === DOWN_KEY) {
             player.facing = "south";
-        } else if (key_id === D_KEY) {
+        } else if (key_id === D_KEY || key_id === RIGHT_KEY) {
             player.facing = "east";
         } else if (key_id === DEBUG_KEY) {
             if (g.debug === true) {
